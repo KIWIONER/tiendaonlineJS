d37ed497f9f849b76ef692ac2fb3069eae0f199c
matiasidiart <matiasidiartviera@gmail.com>
2023-01-02 22:17:38 +0100

    pusheando

---
+++ src/components/data/stock.js
@@ -0,0 +1,98 @@
+const productos =[
+    {
+    id:1, 
+    nombre:'Camisa Abercombi', 
+    precio : 35.00,
+    img:"public/images/camisa-abercombi.jpg",    
+    cantidad: 1,
+    class: 'tarjetaProducto1'
+},
+    {
+    id:2, 
+    nombre:'Camisa Alpha Veta', 
+    precio : 29.00, 
+    img:"public/images/camisa-alpha-veta.jpg",
+    cantidad: 1,
+    class: 'tarjetaProducto2'
+},
+    {
+    id:3,
+    nombre:'Camisa Club Dámingo',
+    precio:31.00, 
+    img: "public/images/camisa-amarilla-cob-damingo.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto3'
+},
+    {
+    id:4,
+    nombre:'Camisa Bombay Store',
+    precio: 35.00,
+    img: "public/images/camisa-bombay-store.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto4'
+},
+    {
+    id:5, 
+    nombre:'Camisa casa moda', 
+    precio:35.00, 
+    img:"public/images/camisa-casa-moda.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto5'
+},
+    {
+    id:6, 
+    nombre:'Camisa casual club', 
+    precio:44.00, 
+    img:"public/images/camisa-casual-club.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto6'
+},
+    {
+    id:7, 
+    nombre:'Camisa goodiellow', 
+    precio:35.00, 
+    img:"public/images/camisa-goodiellow.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto7'
+},
+    {
+    id:8, 
+    nombre:'Camisa happy jack', 
+    precio:35.00, 
+    img:"public/images/camisa-happy-jack.jpg",
+    cantidad:1
+},
+    {
+    id:9, 
+    nombre:'Camisa jagari', 
+    precio:20.00, 
+    img:"public/images/camisa-jagari.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto9'
+},
+    {
+    id:10, 
+    nombre:'Camisa LOGG',
+    precio:21.00, 
+    img:"public/images/camisa-logg.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto10'
+},
+    {
+    id:11, 
+    nombre:'Camisa M&S mode',
+    precio:35.00, 
+    img:"public/images/camisa-ms-mode.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto11'
+},
+    {
+    id:12, 
+    nombre:'Camisa YES', 
+    precio:19.00, 
+    img:"public/images/camisa-yes.jpg",
+    cantidad:1,
+    class: 'tarjetaProducto12'
+}
+];
+
